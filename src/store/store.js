d9e89c922c4278858bddc25725b880294d45fe7c
Nadim Mahmud <nadimlancer0@gmail.com>
2026-01-29 10:23:35 +0600

    Understanding user dashboard

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import recordReducer from "./recordSlice";
-const store = configureStore({
-  reducer: {
-    records: recordReducer,
-  },
-});
-
-export default store;
