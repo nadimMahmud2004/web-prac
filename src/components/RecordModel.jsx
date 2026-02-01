9ac3f9d71770540932af651bf4d59a2b2235c7ee
Nadim Mahmud <nadimlancer0@gmail.com>
2026-02-01 14:06:24 +0600

    Understanding Redux logic

--- src/components/RecordModel.jsx
+++ src/components/RecordModel.jsx
@@ -1,6 +1,51 @@
 import { X } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { updateRecords, addRecord } from "../store/recordSlice";
 
-const RecordModel = () => {
+const RecordModel = ({ isOpen, onClose, currentRecord }) => {
+  const dispatch = useDispatch();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    position: "",
+  });
+
+  useEffect(() => {
+    if (currentRecord) {
+      setFormData(currentRecord);
+    } else {
+      setFormData({
+        name: "",
+        email: "",
+        phone: "",
+        position: "",
+      });
+    }
+  }, [currentRecord]);
+
+  const handleSubmit = () => {
+    if (!formData.name.trim() || !formData.email.trim()) {
+      alert("Name and Email is required !");
+      return;
+    }
+    if (currentRecord) {
+      dispatch(
+        updateRecords({
+          id: currentRecord.id,
+          data: formData,
+        }),
+      );
+    } else {
+      dispatch(addRecord(formData));
+    }
+    onClose();
+  };
+
+  if (!isOpen) return null;
+
   return (
     <div className="fixed inset-0 backdrop-blur-sm flex items-center justify-center p-4 z-50">
       <div className="bg-white rounded-lg shadow-2xl max-w-md w-full">
@@ -9,7 +54,10 @@ const RecordModel = () => {
           <h2 className="text-2xl font-bold text-gray-800">
             Register New Record
           </h2>
-          <button className="text-gray-400 hover:text-gray-600 transition-all cursor-pointer">
+          <button
+            className="text-gray-400 hover:text-gray-600 transition-all cursor-pointer"
+            onClick={onClose}
+          >
             <X size={24} />
           </button>
         </div>
@@ -23,6 +71,10 @@ const RecordModel = () => {
             <input
               type="text"
               placeholder="Enter Your Name"
+              value={formData.name}
+              onChange={(e) =>
+                setFormData({ ...formData, name: e.target.value })
+              }
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
@@ -33,6 +85,10 @@ const RecordModel = () => {
             </label>
             <input
               type="email"
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
               placeholder="Enter Your Email"
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
@@ -44,6 +100,10 @@ const RecordModel = () => {
             </label>
             <input
               type="tel"
+              value={formData.phone}
+              onChange={(e) =>
+                setFormData({ ...formData, phone: e.target.value })
+              }
               placeholder="Enter Your Phone"
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
@@ -56,17 +116,27 @@ const RecordModel = () => {
             <input
               type="text"
               placeholder="Enter Your Position"
+              value={formData.position}
+              onChange={(e) =>
+                setFormData({ ...formData, position: e.target.value })
+              }
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
         </div>
         {/* Footer Button */}
         <div className="flex gap-3 p-6 border-t border-gray-200">
-          <button className="flex-1 px-4 py-2 border border-gray-300 text-gray-700 rounded-lg hover:bg-gray-50 transition-all font-medium hover:scale-105 ">
+          <button
+            className="flex-1 px-4 py-2 border border-gray-300 text-gray-700 rounded-lg hover:bg-gray-50 transition-all font-medium hover:scale-105"
+            onClick={onClose}
+          >
             Cancel
           </button>
-          <button className="flex-1 px-4 py-2 bg-blue-500 border border-gray-300 text-white rounded-lg hover:bg-blue-600 transition-all font-medium hover:scale-105 ">
-            Register
+          <button
+            className="flex-1 px-4 py-2 bg-blue-500 border border-gray-300 text-white rounded-lg hover:bg-blue-600 transition-all font-medium hover:scale-105"
+            onClick={handleSubmit}
+          >
+            {currentRecord ? "Update" : "Register"}
           </button>
         </div>
       </div>
--- src/components/RecordTable.jsx
+++ src/components/RecordTable.jsx
@@ -1,11 +1,52 @@
 import { Edit2, Plus, Search, Trash2 } from "lucide-react";
-import React from "react";
 import RecordModel from "./RecordModel";
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  setSearchTerm,
+  selectAllRecords,
+  selectSearchTerm,
+  deleteRecord,
+  selectFilteredRecords,
+} from "../store/recordSlice";
+import { useState } from "react";
 
 const RecordTable = () => {
+  const dispatch = useDispatch();
+
+  const filteredRecords = useSelector(selectFilteredRecords);
+  const allRecords = useSelector(selectAllRecords);
+  const searchTerm = useSelector(selectSearchTerm);
+
+  const storedRecords =
+    filteredRecords?.slice().sort((a, b) => b.id - a.id) || [];
+
+  const [showModel, setShowModel] = useState(false);
+  const [currentRecord, setCurrentRecord] = useState(null);
+
+  const openCreateModal = () => {
+    setCurrentRecord(null);
+    setShowModel(true);
+  };
+
+  const openEditModal = (record) => {
+    setCurrentRecord(record);
+    setShowModel(true);
+  };
+
+  const closeModal = () => {
+    setShowModel(false);
+    setCurrentRecord(null);
+  };
+
+  const handleDelete = (record) => {
+    dispatch(deleteRecord(record.id));
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 p-6">
       <div className="max-w-7xl mx-auto">
+        {/* Header */}
         <div className="bg-white rounded-lg shadow-md p-6 mb-6">
           <h1 className="text-3xl font-bold text-gray-800 mb-2">
             Employee Management
@@ -27,10 +68,15 @@ const RecordTable = () => {
               <input
                 type="text"
                 placeholder="Search by name , email or position"
+                value={searchTerm}
+                onChange={(e) => dispatch(setSearchTerm(e.target.value))}
                 className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
-            <div className="flex items-center justify-center gap-2 bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition-all">
+            <div
+              className="flex items-center justify-center gap-2 bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition-all"
+              onClick={openCreateModal}
+            >
               <Plus size={20} />
               Add New Record{" "}
             </div>
@@ -65,40 +111,49 @@ const RecordTable = () => {
               </thead>
               <tbody className="divide-y divide-gray-200">
                 {/* Conditional Rendering */}
-
-                <tr className="px-6 py-12 text-center text-gray-500">
-                  <td colSpan={6}>No Records Found</td>
-                </tr>
-                {/* Else */}
-                <tr className="hover:bg-gray-50 transition-colors">
-                  <td className="px-6 py-2 whitespace-nowrap text-sm text-gray-900">
-                    1
-                  </td>
-                  <td className="px-6 py-2 whitespace-nowrap text-sm text-gray-900">
-                    John Doe
-                  </td>
-                  <td className="px-6 py-2 whitespace-nowrap text-sm text-gray-900">
-                    Johndoe@gmail.com
-                  </td>
-                  <td className="px-6 py-2 whitespace-nowrap text-sm text-gray-900">
-                    083438473
-                  </td>
-                  <td className="px-6 py-2 whitespace-nowrap text-sm text-gray-900">
-                    Full stack
-                  </td>
-                  <td className="px-6 py-2 whitespace-nowrap text-sm text-gray-900">
-                    <div className="flex items-center justify-center gap-3">
-                      <button className="flex items-center justify-center gap-1 bg-blue-600 text-white px-3 py-1.5 rounded hover:bg-blue-700 transition-all text-sm font-medium">
-                        <Edit2 size={16} />
-                        Edit
-                      </button>
-                      <button className="flex items-center justify-center gap-1 bg-red-600 text-white px-3 py-1.5 rounded hover:bg-red-700 transition-all text-sm font-medium">
-                        <Trash2 size={16} />
-                        Delete
-                      </button>
-                    </div>
-                  </td>
-                </tr>
+                {storedRecords.length === 0 ? (
+                  <tr className="px-6 py-12 text-center text-gray-500">
+                    <td colSpan={6}>No Records Found</td>
+                  </tr>
+                ) : (
+                  storedRecords.map((record) => (
+                    <tr className="hover:bg-gray-50 transition-colors">
+                      <td className="px-6 py-2 whitespace-nowrap text-sm text-gray-900">
+                        {record.id}
+                      </td>
+                      <td className="px-6 py-2 whitespace-nowrap text-sm text-gray-900">
+                        {record.name}
+                      </td>
+                      <td className="px-6 py-2 whitespace-nowrap text-sm text-gray-900">
+                        {record.email}
+                      </td>
+                      <td className="px-6 py-2 whitespace-nowrap text-sm text-gray-900">
+                        {record.phone}
+                      </td>
+                      <td className="px-6 py-2 whitespace-nowrap text-sm text-gray-900">
+                        {record.position}
+                      </td>
+                      <td className="px-6 py-2 whitespace-nowrap text-sm text-gray-900">
+                        <div className="flex items-center justify-center gap-3">
+                          <button
+                            className="flex items-center justify-center gap-1 bg-blue-600 text-white px-3 py-1.5 rounded hover:bg-blue-700 transition-all text-sm font-medium"
+                            onClick={() => openEditModal(record)}
+                          >
+                            <Edit2 size={16} />
+                            Edit
+                          </button>
+                          <button
+                            className="flex items-center justify-center gap-1 bg-red-600 text-white px-3 py-1.5 rounded hover:bg-red-700 transition-all text-sm font-medium"
+                            onClick={() => handleDelete(record)}
+                          >
+                            <Trash2 size={16} />
+                            Delete
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  ))
+                )}
               </tbody>
             </table>
           </div>
@@ -106,13 +161,19 @@ const RecordTable = () => {
           {/* Footer Showing filtered */}
           <div className="bg-gray-50 px-6 py-3 border-t border-gray-200 ">
             <p className="text-sm text-gray-600 text-center">
-              Showing shorted records of all records
+              Showing {storedRecords.length} of {allRecords.length} records.
             </p>
           </div>
         </div>
       </div>
       {/* Model */}
-      <RecordModel />
+      {showModel && (
+        <RecordModel
+          isOpen={showModel}
+          onClose={closeModal}
+          currentRecord={currentRecord}
+        />
+      )}
     </div>
   );
 };
