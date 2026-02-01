35e17a4f411fa0b431b517daf37944a74791acb9
Nadim Mahmud <nadimlancer0@gmail.com>
2026-02-01 15:51:50 +0600

    Understanding

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import recordReducers from "./recordSlice";
-
-export const store = configureStore({
-  reducer: {
-    records: recordReducers,
-  },
-});
