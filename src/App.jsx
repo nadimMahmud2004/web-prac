57c4106986ff0d8f0d7feda4df4b3c6911cfe9c4
Nadim Mahmud <nadimlancer0@gmail.com>
2026-02-04 17:21:45 +0600

    Understanding design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { Footer } from "./layout/Footer";
 import { Navbar } from "./layout/Navbar";
+import { Contact } from "./sections/Contact";
+import { Experience } from "./sections/Experience";
 import { Hero } from "./sections/Hero";
+import { Projects } from "./sections/Projects";
+import { Testimonials } from "./sections/Testimonial";
 
 function App() {
   return (
@@ -8,6 +12,10 @@ function App() {
       <Navbar />
       <main>
         <Hero />
+        <Projects />
+        <Experience />
+        <Testimonials />
+        <Contact />
       </main>
       <Footer />
     </div>
