e7e202aba43f79c4960b95fa144b5e4d57056c98
Nadim Mahmud <nadimlancer0@gmail.com>
2026-02-14 03:03:54 +0600

    Understanding

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import Features from "./components/Features";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
-import Pricing from "./components/Pricing";
-import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
 
 const App = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      setScrolled(window.scrollY > 50);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
-      <Navbar />
-      <Hero />
-      <Features />
-      <Pricing />
-      <Testimonials />
+      <Navbar scrolled={scrolled} />
       <Footer />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
-
 import logo from "../assets/logo.png";
 
 export default function Navbar({ scrolled }) {
@@ -8,28 +7,24 @@ export default function Navbar({ scrolled }) {
 
   return (
     <nav
-      className={`fixed top-0 w-full z-50 transition-all duration-300 ${scrolled ? "bg-slate-950/80 backdrop-blur-lg border-b border-slate-800" : "bg-slate-950/20 backdrop-blur-sm"}`}
+      className={`fixed top-0 w-full z-50 transition-all duration-300 ${scrolled ? "bg-slate-950/80 backdrop-blur-2xl border-b border-slate-800" : "bg-slate-950/20 backdrop-blur-sm"}`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-14 sm:h-16 md:h-20">
           <div className="flex items-center space-x-1 group cursor-pointer">
             <div>
-              <img
-                src={logo}
-                alt="CodeFlow"
-                className="w-6 h-6 sm:w-8 sm:h-8"
-              />
+              <img src={logo} className="w-6 h-6 sm:w-8 sm:h-8" />
             </div>
 
             <span className="text-lg sm:text-xl md:text-2xl font-medium">
               <span className="text-white">Code</span>
-              <span className="text-blue-400">Flow</span>
+              <span className="text-blue-400">Code</span>
             </span>
           </div>
 
-          {/* NavLinks */}
+          {/* Nav links */}
 
-          <div className="hidden md:flex items-center space-x-6 lg:space-x-8">
+          <div className="hidden md:flex items-center space-x-6 lg:space-x-6">
             <a
               href="#features"
               className="text-gray-300 hover:text-white text-sm lg:text-base"
