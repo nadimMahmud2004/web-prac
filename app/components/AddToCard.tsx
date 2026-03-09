1a69e127365fc0b4639a26dbb6bf3e7b8cbcf002
Nadim Mahmud <nadimlancer0@gmail.com>
2026-03-09 18:17:05 +0800

    Understanding

---
+++ app/components/AddToCard.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const AddToCard = () => {
+  return (
+    <div>
+      <button onClick={() => console.log("click")}>Add to Cart</button>
+    </div>
+  );
+};
+
+export default AddToCard;
---
+++ app/users/new/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UsersNewPages = () => {
+  return (
+    <div>
+      <h1>UsersNewPages</h1>
+    </div>
+  );
+};
+
+export default UsersNewPages;
